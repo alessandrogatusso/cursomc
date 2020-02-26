fea052eb0d735136e4728ed8571f52d5c1536c76
Alessandro Sampaio <alessandrossampaio@gmail.com>
2020-02-26 19:19:14 -0300

    Criando operacao de instanciacao

---
+++ src/main/java/com/sampaio/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.sampaio.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sampaio.cursomc.domain.Categoria;
+import com.sampaio.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
